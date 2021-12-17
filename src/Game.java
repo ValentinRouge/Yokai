237ca63c7c65aa039dfd239b42c6534c78f3a421
ChloePezzoni <chloe.pezzoni@outlook.com>
2021-12-17 14:47:56 +0100

    Creation des classes

--- src/Game.java
+++ src/Game.java
@@ -3,4 +3,8 @@ public class Game {
         YokaiCard carte1 = new YokaiCard(Families.Rokurokubi);
         System.out.println(carte1.getFamily());
     }
+    public static void test2(){
+        Player name1 = new Player("Valentin");
+        System.out.println(name1.getName());
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,14 @@
 public class Player {
+    private final String name; // on declare un attribut
+
+    public Player(String name) { // constructeur si je creer une personne je lui donne un nom
+        this.name = name;
+    }
+
+    public String getName() { //  getter
+        return name;
+    }
+
+
+
 }
