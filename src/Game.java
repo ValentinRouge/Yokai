e4e57acc24a286dc87a33be9d8b346df21916554
ValentinRouge <rougevalentin@outlook.com>
2021-12-21 22:05:32 +0100

    Créer indices 2 couleur

--- src/Game.java
+++ src/Game.java
@@ -3,14 +3,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-    private ArrayList<HintCard> listOfHintCardHidden = new ArrayList<HintCard>();
-    private ArrayList<HintCard> listOfHintCardAvailable = new ArrayList<HintCard>();
-    private ArrayList<YokaiCard> listOfYokaiCard = new ArrayList<YokaiCard>();
-    private ArrayList<Player> players = new ArrayList<Player>();
+    private ArrayList<HintCard> listOfHintCardHidden = new ArrayList<>();
+    private ArrayList<HintCard> listOfHintCardAvailable = new ArrayList<>();
+    private ArrayList<YokaiCard> listOfYokaiCard = new ArrayList<>();
+    private ArrayList<Player> players = new ArrayList<>();
     private boolean gameInAction;
     private Board board;
 
     public void BeginGame(){
+        gameInAction = true;
         CreatePlayers();
         CreateHints();
     }
@@ -24,9 +25,9 @@ public class Game {
 
     private void CreateHints(){
         Random random = new Random();
-        ArrayList<Integer> HintNeeded = new ArrayList<Integer>();
-        switch (players.size()){ //Le switch case ne sert à rien mais il sera utile si on décide d'ouvrir le jeu à plus que 2 joueurs
-            case 2: {
+        ArrayList<Integer> HintNeeded = new ArrayList<>();
+        switch (players.size()) { //Le switch case ne sert à rien mais il sera utile si on décide d'ouvrir le jeu à plus que 2 joueurs
+            case 2 -> {
                 HintNeeded.add(2);
                 HintNeeded.add(3);
                 HintNeeded.add(2);
@@ -42,6 +43,18 @@ public class Game {
             FamilyList.remove(family);
         }
 
+        ArrayList<String> DoubleHintAvailable = new ArrayList<>();
+        for (int i = 0; i<4; i++){ // génération des doublets possibles
+            for (int y=i; y<4; y++){
+                if (i != y) DoubleHintAvailable.add(i+"-"+y);
+            }
+        }
+        for (int i=0; i<HintNeeded.get(1);i++){ //on en prend dans notre liste prégénérée et on les ajoutes à notre liste
+            int index = random.nextInt(DoubleHintAvailable.size());
+            String[] families = DoubleHintAvailable.get(index).split("-");
+            listOfHintCardHidden.add(new HintCard(Families.valueOf(Integer.parseInt(families[0])), Families.valueOf(Integer.parseInt(families[1]))));
+            DoubleHintAvailable.remove(index);
+        }
 
         FamilyList.clear(); //on recréer la liste avec toutes les familles
         for (int i=0; i<4; i++) FamilyList.add(i);
